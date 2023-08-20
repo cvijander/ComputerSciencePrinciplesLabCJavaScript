a69d7d012e438cbd0acf376bff9fc60d748b118a
cvijander <cvija85@gmail.com>
2023-08-20 19:05:41 +0200

    Functions

--- script.js
+++ script.js
@@ -2,8 +2,28 @@
 
 //Define a variable with integer value 
 
-var myNumber =5;
+  //var myNumber =5;
 
 //Send the value of the variable to the output console
-console.log(myNumber);
-console.log(typeof myNumber);
+//console.log(myNumber);
+//console.log(typeof myNumber); 
+
+//create the game score
+var myScore ;
+
+//set up and chenge the game score 
+init();
+changeScore();
+
+//function to initalize the game score in the game
+function init()
+{
+myScore = 1000;
+}
+
+// function to change the score of the game
+function changeScore()
+{
+    myScore = myScore +100;
+    console.log("Players score: " + myScore);
+}
