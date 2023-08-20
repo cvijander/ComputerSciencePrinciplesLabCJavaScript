208b0ec642007a1ca360d5a636b63afe83551510
cvijander <cvija85@gmail.com>
2023-08-20 18:55:32 +0200

    First example

---
+++ script.js
@@ -0,0 +1,9 @@
+// JavaScript Document
+
+//Define a variable with integer value 
+
+var myNumber =5;
+
+//Send the value of the variable to the output console
+console.log(myNumber);
+console.log(typeof myNumber);
