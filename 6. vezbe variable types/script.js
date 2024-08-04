e6a49aa819b4c6cabdc14dceb65c88e5248a3cd0
cvijander <cvija85@gmail.com>
2024-08-04 23:42:37 +0200

    Variable types

---
+++ 6. vezbe variable types/script.js
@@ -0,0 +1,18 @@
+// javascript document
+
+var myVariable =5;
+var msg = "myVariable is equal to " +myVariable + " and is a " +typeof myVariable;
+console.log(msg); 
+
+myVariable =myVariable.toString();
+var msg = "myVariable is equal to " +myVariable + " and is a " +typeof myVariable;
+console.log(msg); 
+
+myVariable +=1;
+var msg = "myVariable is equal to " +myVariable + " and is a " +typeof myVariable;
+console.log(msg); 
+
+myVariable =Number(myVariable)+1;
+var msg = "myVariable is equal to " +myVariable + " and is a " +typeof myVariable;
+console.log(msg); 
+
