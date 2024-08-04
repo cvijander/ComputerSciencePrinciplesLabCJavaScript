c16ff550fd958eb63e4a813734abfac04f158422
cvijander <cvija85@gmail.com>
2024-08-05 00:47:18 +0200

    while and do while loops

---
+++ 9. vezbe while/script.js
@@ -0,0 +1,18 @@
+var myValue = 0;
+
+while (myValue < 100) 
+{
+    if(myValue % 9 ==0)
+    {
+        console.log(myValue);
+    }
+    myValue++;
+}
+
+var myBadValue = 0;
+
+do {
+    console.log("This will run at least once. ");
+} while (myBadValue !=0 )
+
+    
