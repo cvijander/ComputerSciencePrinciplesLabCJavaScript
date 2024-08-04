e9984d49998d18a8041bfda269f50663d04dc3bc
cvijander <cvija85@gmail.com>
2024-08-05 00:14:08 +0200

    Condition test if else

---
+++ 7. vezbe conditional tests/script.js
@@ -0,0 +1,57 @@
+
+var knowJavaScript =true;
+console.log("The variable knowJavascipt is equal to " + knowJavaScript + " and is a " + typeof knowJavaScript);
+
+if(knowJavaScript) {
+    console.log("I know Javascript");
+}
+
+var myValue = 5;
+console.log("myValue is equal to 5");
+
+if(myValue ==5) {
+    console.log("This condition (myValue==5) is true. ");
+
+}
+
+var myValue =5;
+console.log("myValue is equal to 5");
+
+if(myValue < 5) 
+{
+    console.log("This condition (myValue < 5) is true");
+}
+else 
+{
+    console.log("This condition (myValue < 5) is false ");
+}
+
+
+var myValue =5;
+console.log("myValue is equal to 5");
+
+if (myValue != 5) {
+    console.log("This condition (myValue != 5) is true")
+}
+else if (myValue >5 ){
+console.log("This condition (myValue >5 ) is true");
+
+} else 
+{
+    console.log("All conditions are false");
+}
+
+
+
+var myValue =5;
+console.log("myValue is equal to 5");
+
+if(myValue >=1) 
+{
+    console.log("This condition >=1 is true");
+
+    if(myValue <=5 )
+    {
+        console.log("This condition (myValue <=5) is true");
+    }
+}
